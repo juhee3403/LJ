c1ebd5f30b974000c3e91b368243de92b3cd7930
Juhee Lee <juhee3403@hotmail.com>
2018-01-25 21:44:08 +0900

    use doubleqoutes

--- es6/test.js
+++ es6/test.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 // es6 기능: 블록 스코프 변수 선언
 const sentences = [
-    {subject : 'Javascript', verb : 'is', object : 'great'},
-    {subject : 'Elephants', verb : 'are', object : 'large'},
+    {subject : "Javascript", verb : "is", object : "great"},
+    {subject : "Elephants", verb : "are", object : "large"},
 ];
 // es6 기능: 객체 분해
 function say({subject, verb, object}) {
