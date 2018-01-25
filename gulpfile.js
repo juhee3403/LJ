8e2f7803033db7bcf854b75f70c6332901236564
Juhee Lee <juhee3403@hotmail.com>
2018-01-25 20:04:43 +0900

    connect gulp & babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 // 걸프의 의존성을 여기 쓴다.
 
 gulp.task('default', function() {
     // 걸프 작업을 여기에 쓴다.
+    // 노드 소스
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+
+    // 브라우저 소스
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
