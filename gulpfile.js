c3b72bbe93a42bf24c4c30f8582a49814d52fabf
Juhee Lee <juhee3403@hotmail.com>
2018-01-25 21:06:26 +0900

    link gulp-ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 // 걸프의 의존성을 여기 쓴다.
 
 gulp.task('default', function() {
     // 걸프 작업을 여기에 쓴다.
+
+    // ESLint를 실행합니다.
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+
     // 노드 소스
     gulp.src("es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 
     // 브라우저 소스
     gulp.src("public/es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("public/dist"));
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
 });
