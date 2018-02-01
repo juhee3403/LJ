4a9594215186107d0ea23c29fb6c0b444154784c
Juhee Lee <juhee3403@hotmail.com>
2018-02-01 13:15:06 +0900

    내용 추가

---
+++ bool.js
@@ -0,0 +1,10 @@
+
+// 불리언 (boolean)
+// 불리언은 true와 false 두 가지 값밖에 없는 데이터 타입니다.
+
+// C같은 언어는 불리언 대신 숫자를 사용한다.
+// 0은 false고, 다른 숫자는 모두 ture.
+
+let heating = true;
+let cooling = false;
+let cooling2 = "false"; // "false"문자열은 true가 된다.
