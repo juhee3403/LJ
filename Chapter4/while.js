a1d2a8d75f5d114c1e365ca4dbf69fbf761efd20
Juhee Lee <juhee3403@hotmail.com>
2018-02-02 13:15:20 +0900

    내용 추가

--- Chapter4/while.js
+++ Chapter4/while.js
@@ -10,3 +10,12 @@ while (funds > 1 && funds < 100) {
 
 }
 
+function rand(m, n) {
+    return m + Math.floor((n - m + 1)*Math.random());
+}
+
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
+        [rand(0, 5)]
+}
+
